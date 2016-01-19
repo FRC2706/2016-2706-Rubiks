1857e8f5ae7cb8777462ada930f37856345c133e
eandr127 <eandr127@outlook.com>
2016-01-18 20:54:04 -0500

    Initial Commit, Includes joystick support *Untested*

---
+++ src/org/usfirst/frc/team2706/robot/RobotMap.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team2706.robot;
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+	
+	// Left gearbox
+	public static final int MOTOR_FRONT_LEFT = 3;
+	// Right gearbox
+	public static final int MOTOR_FRONT_RIGHT = 0;
+	// Right gearbox
+	public static final int MOTOR_REAR_RIGHT = 1;
+	// Left gearbox
+	public static final int MOTOR_REAR_LEFT = 2;
+	
+	public static final int CAN_ADDR_PNEUMATIC = 0;
+	
+	// @TODO: Get encoder channels, gyro channel, and rangefinder channel
+}
